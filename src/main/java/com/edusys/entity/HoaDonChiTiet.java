b9089b3fc6d08a59f2f990523400a9bde726c6aa
ADMIN <ADMIN@THUAN-FY>
2024-07-08 17:05:45 +0700

    HoaDonChiTiet

---
+++ src/main/java/com/edusys/entity/HoaDonChiTiet.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.edusys.entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class HoaDonChiTiet {
+    private String maHDCT;
+    private String maHD;
+    private String maSanPham;
+    private int soLuong;
+    private float donGia;
+    private float thanhTien;
+
+    public HoaDonChiTiet() {
+    }
+
+    public HoaDonChiTiet(String maHDCT, String maHD, String maSanPham, int soLuong, float donGia, float thanhTien) {
+        this.maHDCT = maHDCT;
+        this.maHD = maHD;
+        this.maSanPham = maSanPham;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.thanhTien = thanhTien;
+    }
+
+    public String getMaHDCT() {
+        return maHDCT;
+    }
+
+    public void setMaHDCT(String maHDCT) {
+        this.maHDCT = maHDCT;
+    }
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getMaSanPham() {
+        return maSanPham;
+    }
+
+    public void setMaSanPham(String maSanPham) {
+        this.maSanPham = maSanPham;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public float getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(float donGia) {
+        this.donGia = donGia;
+    }
+
+    public float getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThanhTien(float thanhTien) {
+        this.thanhTien = thanhTien;
+    }
+    
+    
+}
