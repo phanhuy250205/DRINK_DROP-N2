cf0520c2f57af138d47f963d3cfa6555f353c3a7
Phan Huy <162803292+phanhuy250205@users.noreply.github.com>
2024-07-08 16:28:30 +0700

    Update Giaohangdao.java

--- src/main/java/com/edusys/dao/Giaohangdao.java
+++ src/main/java/com/edusys/dao/Giaohangdao.java
@@ -27,58 +27,6 @@ public class Giaohangdao extends MainDao<Giaohang , String>{
     final String all_SQl="select * from GIAOHANG";
     final String select_By_id_SQl="select * from GIAOHANG WHERE MaGiaoHang = ?";
 
-//    @Override
-//    public void insert(Giaohang entity) {
-//        JDBChelper.update(insert_SQl, entity.getMaGiaoHang(), entity.getLoaiNuoc(),entity.getMaSP(),entity.getDiaChiGiao(), entity.getTenNguoiGiao(),entity.getSDTNguoiGiao(),entity.getTenNguoiNhan(),entity.getSDTNguoiNhan(),entity.getTinhTrang());
-//    }
-//
-//    @Override
-//    public void update(Giaohang entity) {
-//        JDBChelper.update(update_SQl, entity.getLoaiNuoc(),entity.getMaSP(),entity.getDiaChiGiao(),entity.getTenNguoiGiao(),entity.getSDTNguoiGiao(),entity.getTenNguoiNhan(),entity.getSDTNguoiNhan(),entity.getTinhTrang(),entity.getMaGiaoHang());
-//    }
-//
-//    @Override
-//    public void delete(String id) {
-//       JDBChelper.update(delete_SQl, id);
-//    }
-//
-//    @Override
-//    public List<Giaohang> selectAll() {
-//       return  selectBySQL(all_SQl);
-//    }
-//
-//    @Override
-//    public Giaohang selectbyid(String id) {
-//       List<Giaohang> list =selectBySQL(select_By_id_SQl, id);
-//        if (list.isEmpty()) {
-//            return null;
-//        }
-//        return list.get(0);
-//    }
-//
-//    @Override
-//    public List<Giaohang> selectBySQL(String sql, Object... args) {
-//         List<Giaohang> list = new ArrayList<>();
-//        try {
-//            ResultSet rs = JDBChelper.querry(sql, args);
-//            while (rs.next()) {                
-//                Giaohang ennity = new Giaohang();
-//                ennity.setMaGiaoHang(rs.getString("MaGiaoHang"));
-//                ennity.setLoaiNuoc(rs.getString("LoaiNuoc"));
-//                ennity.setMaSP(rs.getString("MaSP"));
-//                ennity.setDiaChiGiao(rs.getString("DiaChiGiao"));
-//                ennity.setTenNguoiGiao(rs.getString("TenNguoiGiao"));
-//                ennity.setSDTNguoiGiao(rs.getString("SDTNguoiGiao"));
-//                ennity.setTenNguoiNhan(rs.getString("TenNguoiNhan"));
-//                ennity.setSDTNguoiNhan(rs.getString("SDTNguoiNhan"));
-//                ennity.setTinhTrang(rs.getString("TinhTrangGiaoHang"));
-//                list.add(ennity);
-//            }
-//        } catch (Exception e) {
-//            System.out.println("lo" + e);
-//        }
-//         return  list;
-//    }
 
     @Override
     public void insert(Giaohang entity) {
