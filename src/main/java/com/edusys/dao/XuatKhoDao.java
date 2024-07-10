c21d4a156b4e8da9f55c16a8484b5219f7f53694
ASUS <ASUS@VODINHHOI>
2024-07-10 15:37:13 +0700

    hoi

---
+++ src/main/java/com/edusys/dao/XuatKhoDao.java
@@ -0,0 +1,67 @@
+package com.edusys.dao;
+
+import com.edusys.entity.XuatKho;
+import com.edusys.utils.JdbcHelper;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XuatKhoDao extends DrinkdropDao<XuatKho, String> {
+
+    final String INSERT_SQL = "INSERT INTO XuatKho(MaPhieuXuat,MaSanPham,TenSanPham,SoLuong,NgayXuat,MaNguoiXuat,GhiChu)VALUES(?,?,?,?,?,?,?)";
+    final String UPDATE_SQL = "UPDATE XuatKho SET MaSanPham = ? ,TenSanPham = ? ,SoLuong = ? ,NgayXuat = ? ,MaNguoiXuat = ? ,GhiChu = ? WHERE MaPhieuXuat = ?";
+    final String DELETE_SQL = "DELETE FROM XuatKho WHERE MaPhieuXuat = ?";
+    final String SELECT_ALL_SQL = "SELECT * FROM XuatKho";
+    final String SELECT_BY_ID_SQL = "SELECT * FROM XuatKho WHERE MaPhieuXuat = ?";
+
+    @Override
+    public void insert(XuatKho entity) {
+        JdbcHelper.update(INSERT_SQL, entity.getMaPhieuXuat(), entity.getMaSanPham(), entity.getTenSanPham(), entity.getSoLuong(), entity.getNgayXuat(), entity.getMaNguoiXuat(), entity.getGhiChu());
+    }
+
+    @Override
+    public void update(XuatKho entity) {
+        JdbcHelper.update(UPDATE_SQL, entity.getMaSanPham(), entity.getTenSanPham(), entity.getSoLuong(), entity.getNgayXuat(), entity.getMaNguoiXuat(), entity.getGhiChu(), entity.getMaPhieuXuat());
+    }
+
+    @Override
+    public void delete(String id) {
+        JdbcHelper.update(DELETE_SQL, id);
+    }
+
+    @Override
+    public List<XuatKho> selectAll() {
+        return selectBySql(SELECT_ALL_SQL);
+    }
+
+    @Override
+    public XuatKho selectById(String id) {
+        List<XuatKho> list = selectBySql(SELECT_BY_ID_SQL, id);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public List<XuatKho> selectBySql(String sql, Object... args) {
+        List<XuatKho> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql, args);
+            while (rs.next()) {
+                XuatKho entity = new XuatKho();
+                entity.setMaPhieuXuat(rs.getString("MaPhieuXuat"));
+                entity.setMaSanPham(rs.getString("MaSanPham"));
+                entity.setTenSanPham(rs.getString("TenSanPham"));
+                entity.setSoLuong(rs.getInt("SoLuong"));
+                entity.setNgayXuat(rs.getDate("NgayXuat"));
+                entity.setMaNguoiXuat(rs.getString("MaNguoiXuat"));
+                entity.setGhiChu(rs.getString("GhiChu"));
+                list.add(entity);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+}
